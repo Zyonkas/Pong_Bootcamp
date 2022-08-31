20d838216ab988eb1cd3b8126d3d61898ccee4df
Zyonkas <zhujiankai25@gmail.com>
2022-08-31 12:33:38 +0200

    Raquetas moviles y limitando bordes

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from turtle import color
 import pygame as pg
 from objects import Pelota ,raqueta
 
@@ -6,18 +5,27 @@ pg.init()
 
 pantalla_principal = pg.display.set_mode((800,600))
 pg.display.set_caption("Pong")
+cronometro = pg.time.Clock()
 
 game_over = False
 Pelota = Pelota(400, 300, color=(255,255,255))
 raqueta1 = raqueta(20, 300, w=20, h=120, color=(255,255,255))
 raqueta2 = raqueta(780, 300, w=20, h=120, color=(255,255,255))
+raqueta2.vy = 5
+raqueta1.vy = 5
+
 while not game_over:
+    dt = cronometro.tick(60)
+
     #Eventos que have el usuario y devuelve una lista de eventos
     for evento in pg.event.get():
         if evento.type == pg.quit:
-            game_over = True
+            game_over = True 
+    
+    raqueta2.mover(pg.K_UP, pg.K_DOWN)           
+    raqueta1.mover(pg.K_LSHIFT, pg.K_LCTRL)
 
-    pantalla_principal.fill((0,255,0))
+    pantalla_principal.fill((0,0,0))
     Pelota.draw(pantalla_principal)
     raqueta1.draw(pantalla_principal)
     raqueta2.draw(pantalla_principal)
